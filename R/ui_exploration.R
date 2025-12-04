431846c7ff3e72444bfaab4452a586a9537aeb0c
ConradLinusMuh <cmuhirwe@harmonia.com>
2025-12-04 11:53:28 -0500

    Refactor exploration module: align UI variable inputs with fd_basket dataset, update server logic, and add explicit global.R sourcing in app.R

--- R/ui_exploration.R
+++ R/ui_exploration.R
@@ -1,23 +1,41 @@
 # R/ui_exploration.R
 
 ui_exploration <- sidebarLayout(
+
+  # Sidebar Filters ----------------------------------------------------
   sidebarPanel(
     h4("Filters"),
+
+    # Multi-state filter
     selectInput(
       "state_select", "Select State(s):",
       choices = NULL, multiple = TRUE
     ),
+
+    # Year range filter
     sliderInput(
       "year_range", "Select Year Range:",
-      min = 2010, max = 2025, value = c(2020, 2025), sep = ""
+      min = 2010, max = 2025,
+      value = c(2019, 2023), sep = ""
     ),
+
+    # Actual variables from fd_basket
     checkboxGroupInput(
-      "variables", "Socioeconomic Indicators:",
-      choices = c("PovertyRate", "MedianIncome", "UnemploymentRate"),
-      selected = "PovertyRate"
+      "variables", "Food Insecurity Indicators:",
+      choices = c(
+        "Overall Food Insecurity Rate"   = "overall_food_insecurity_rate",
+        "Child Food Insecurity Rate"     = "child_food_insecurity_rate",
+        "Cost per Meal"                  = "cost_per_meal",
+        "SNAP Threshold"                 = "snap_threshold",
+        "Rural/Urban Code (2013)"        = "rural_urban_continuum_code_2013"
+      ),
+      selected = "overall_food_insecurity_rate"
     ),
+
     actionButton("update_map", "Update Map", class = "btn-primary")
   ),
+
+  # Main Panel ---------------------------------------------------------
   mainPanel(
     tabsetPanel(
       tabPanel("Map", leaflet::leafletOutput("map_view", height = "550px")),
