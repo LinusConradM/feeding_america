0ad93c792ad03b6aab03c9514ca581ad667fd0e6
ConradLinusMuh <cmuhirwe@harmonia.com>
2026-02-19 20:59:49 -0500

    Updating Requirements.txt file

--- get_html.py
+++
@@ -1,6 +0,0 @@
-from streamlit.testing.v1 import AppTest
-from bs4 import BeautifulSoup
-
-at = AppTest.from_file('app.py').run()
-soup = BeautifulSoup(at.main[0].value if at.main else "", "html.parser")
-print("OK")
